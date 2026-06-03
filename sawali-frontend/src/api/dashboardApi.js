f3272d31adc444a658cf387529f179e7e05b133d
Your Name <[prajwalpawar3p@gmail.com]>
2026-06-03 12:15:44 +0530

    Add Netlify redirects

--- sawali-frontend/src/api/dashboardApi.js
+++ sawali-frontend/src/api/dashboardApi.js
@@ -1,28 +1,29 @@
 // src/api/dashboardApi.js
-import axios from "axios";
+// import axios from "axios";
+import api from "./api"; 
 
 export const getTodaySummary = async () => {
-  const res = await axios.get("/api/sales/summary-today");
+  const res = await api.get("/api/sales/summary-today");
   return res.data;
 };
 
 export const getPendingSummary = async () => {
-  const res = await axios.get("/api/customers/summary-pending");
+  const res = await api.get("/api/customers/summary-pending");
   return res.data;
 };
 
 export const getRecentSales = async () => {
-  const res = await axios.get("/api/sales/recent");
+  const res = await api .get("/api/sales/recent");
   return res.data;
 };
 
 
 export const getLast7Days = async () => {
-  const res = await axios.get("/api/sales/last7days");
+  const res = await api.get("/api/sales/last7days");
   return res.data;
 };
 
 export const getTopProducts = async () => {
-  const res = await axios.get("/api/sales/top-products");
+  const res = await api.get("/api/sales/top-products");
   return res.data;
 };
